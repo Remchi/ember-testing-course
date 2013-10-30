1c3a2ea93b378e249fafd1543d38639107cbaab9
Rem Zolotykh <remchi@gmail.com>
2013-10-31 00:59:46 +0200

    Feats depend on character race

--- js/app.js
+++ js/app.js
@@ -4,3 +4,26 @@ App.Router.map(function() {
   this.route("character");
 });
 
+App.CharacterRoute = Ember.Route.extend({
+  model: function() {
+    return App.Character.create();
+  }
+});
+
+App.CharacterController = Ember.ObjectController.extend({
+  races: [ "", "elf", "dwarf" ]
+});
+
+App.Character = Ember.Object.extend({
+  race: "",
+
+  feats: function() {
+    if (this.get("race") === "elf") {
+      return [ "Slender", "Night Vision" ];
+    } else if (this.get("race") === "dwarf") {
+      return [ "Brawny" ];
+    }
+    return [];
+  }.property("race")
+});
+
--- js/tests.js
+++ js/tests.js
@@ -15,8 +15,29 @@ test("No feats by default", function() {
   });
 });
 
+test("Choosing race popupates feats list", function() {
+  visit("/character")
+    .fillIn("#select-race", "elf")
+    .then(function() {
+      equal(find("ul#character-feats > li").length, 2, "should have two feats");
+    });
+});
+
 test("Display welcome message on main page", function () {
   visit("/").find("h1").then(function(el) {
     equal(el.length, 1, "H1 tag must be present");
   });
+});
+
+module("Unit Test");
+
+test("Character's feats depends on race", function() {
+  var character = App.Character.create();
+  equal(character.get("feats").length, 0, "no race - no feats");
+
+  character.set("race", "elf");
+  equal(character.get("feats").length, 2, "elf's got two feats");
+
+  character.set("race", "dwarf");
+  equal(character.get("feats").length, 1, "dwarf's got one feat");
 });
